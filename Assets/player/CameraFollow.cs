80089bb5e06c3b3dd1b0cc4f05bc20c552679628
Gabriel Kulp <gabrielkulp@gmail.com>
2018-03-29 14:04:14 -0700

    Added greybox world, simple character
    There's now a boxy world for a capsule to run and jump in. The camera moves to follow the player. I also changed some settings to properly brand this as OGPC.

---
+++ Assets/player/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform follow;
+	public Vector3 positionOffset;
+	public float lerpRate = 5f;
+
+	void Start () {
+		if (follow == null) {
+			Debug.LogWarning("CameraFollow on " + gameObject.name + " has no follow assigned.");
+			return;
+		}
+		transform.position = follow.position + positionOffset;
+	}
+	
+	void Update () {
+		if (follow == null)
+			return;
+		transform.position = Vector3.Lerp(
+			transform.position,
+			follow.position + positionOffset,
+			lerpRate * Time.deltaTime
+		);
+	}
+}
---
+++ Assets/player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public float jumpSpeed = 10f;
+	public float gravity = 20f;
+	[HideInInspector]
+	public CharacterController cc;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		if (cc.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = moveDirection * moveSpeed;
+			transform.LookAt(transform.position + moveDirection, Vector3.up);
+			if (Input.GetButtonDown("Jump"))
+				moveDirection.y = jumpSpeed;
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		cc.Move(moveDirection * Time.deltaTime);
+	}
+}
